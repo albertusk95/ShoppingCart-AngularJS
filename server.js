9ff46900ccd861ae2aa7539730589d7a2477461e
albertusk95 <albertusk15@gmail.com>
2016-07-12 22:10:40 +0700

    updated server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
@@ -12,7 +14,7 @@ app.get('/',function(req,res){
     res.sendFile('main.html',{'root':__dirname + '/public'});
 });
 
-app.listen('8080',function(){
-    console.log('Server is running at port 8080...');
+app.listen(app.get('port'),function(){
+    console.log('Server is running at port ', app.get('port'));
 });
 
